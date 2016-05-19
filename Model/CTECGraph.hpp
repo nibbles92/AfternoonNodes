5505b77e03653fbf272cf3b4387efed3a5c8644d
nibbles92 <droc6148@740s-pl-1176.local>
2016-05-19 13:31:33 -0600

    format it

--- Model/CTECGraph.hpp
+++ Model/CTECGraph.hpp
@@ -7,57 +7,34 @@
 //
 
 #ifndef CTECGraph_hpp
-
 #define CTECGraph_hpp
-
 #include <set>
-
 #include <iostream>
 
 template<class Type>
-
 class CTECGraph
-
 {
-    
 private:
-    
     static const int MAXIMUM = 20;
-    
     bool adjacencyMatrix [MAXIMUM] [MAXIMUM];
-    
     Type labels[MAXIMUM];
-    
     int manyVertices;
-    
     void depthFirstTraversal(CTECGraph<Type> graph, int vertex, bool markedVertecies[]);
     
     
     
 public:
-    
     CTECGraph();
-    
     ~CTECGraph();
-    
     void addVertex(const Type& value);
-    
     void addEdge(int source, int target);
-    
     void removeEdge(int source, int target);
-    
     Type& operator [] (int vertex);
-    
     Type operator [] (int vertex) const;
-    
     int size() const;
-    
     bool isEdge(int source, int target)const;
-    
     std::set<int> neighbors(int vertex) const;
-    
     void depthFirstTraversal(CTECGraph<Type> graph, int vertex);
-    
     void breadthFirstTraversal(CTECGraph<Type> graph, int vertex);
     
 };
